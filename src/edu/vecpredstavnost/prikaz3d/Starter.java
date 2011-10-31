595307127dd2e3ed322167480ea25c49d292de6b
Marko Dolar <marko.dolar@gmail.com>
2011-10-31 12:02:01 +0100

    prvi commit

---
+++ src/edu/vecpredstavnost/prikaz3d/Starter.java
@@ -0,0 +1,12 @@
+package edu.vecpredstavnost.prikaz3d;
+
+import com.badlogic.gdx.backends.jogl.JoglApplication;
+
+public class Starter
+{
+	public static void main(String[] args)
+	{
+		new JoglApplication(new TestClass(), "Hello", 480, 320, false);
+	}
+
+}
